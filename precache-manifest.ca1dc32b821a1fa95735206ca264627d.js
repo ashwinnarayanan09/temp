e0a38c51f7fda1d85a9913c28e2167796f07aa5e
ashwin <ashwinnarayanan09@gmail.com>
2020-03-25 16:19:23 +0100

    first commit

---
+++ precache-manifest.ca1dc32b821a1fa95735206ca264627d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "684973c5852315491b4805f03b8aebde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1abec9d1834fd0e2149",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "b402b3f1c8df33548ceb",
+    "url": "/static/css/main.092608db.chunk.css"
+  },
+  {
+    "revision": "a1abec9d1834fd0e2149",
+    "url": "/static/js/2.4458568c.chunk.js"
+  },
+  {
+    "revision": "39229a1bcf6e43918c713ee185662f93",
+    "url": "/static/js/2.4458568c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b402b3f1c8df33548ceb",
+    "url": "/static/js/main.f3797927.chunk.js"
+  },
+  {
+    "revision": "fcffabf79d4430451355",
+    "url": "/static/js/runtime-main.19d5c850.js"
+  }
+]);
